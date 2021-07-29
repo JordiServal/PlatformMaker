47d39759edfe2e1a5990344f6c59c7aa923b49a5
JordiSerVal <sernavalero96@gmail.com>
2021-07-30 00:31:57 +0200

    Press R to reload scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+ using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)){
+            Scene scene = SceneManager.GetActiveScene(); 
+            SceneManager.LoadScene(scene.name);
+        }
     }
 }
