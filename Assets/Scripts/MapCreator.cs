0a536e4e71c49a28d36c5babb0f1c0a3aff54b50
JordiSerVal <sernavalero96@gmail.com>
2021-08-01 14:49:17 +0200

    Fix map creation curson bug and destroy block on reset

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -10,14 +10,15 @@ public class MapCreator : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hitData = Physics2D.Raycast(new Vector2(worldPosition.x, worldPosition.y), Vector2.zero, 0);
-        if (hitData && Input.GetMouseButtonDown(0)) {
-            Debug.Log(worldPosition);
-
-            // Debug.Log(hitData.transform.gameObject.name);
-            Vector3 pos = new Vector3(Mathf.Floor(worldPosition.x), Mathf.Floor(worldPosition.y), -1);
-            Debug.Log(pos);
-            Debug.Log(blocks[0].tag);
+        RaycastHit2D[] hitData = Physics2D.RaycastAll(new Vector2(worldPosition.x, worldPosition.y), Vector2.zero, 0);
+        if (hitData.Length > 0 && Input.GetMouseButtonDown(0)) {
+			foreach (RaycastHit2D hit in hitData) {
+                if (hit.transform.gameObject.layer ==  6) {
+                    Destroy(hit.transform.parent.gameObject);
+                }
+            }
+            Vector3 pos = new Vector3((int) worldPosition.x, (int) worldPosition.y, -1);
+            // Debug.Log(pos);
             Instantiate(blocks[0], pos, Quaternion.identity);
         }
     }
